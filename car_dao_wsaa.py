0bcf33f93be65c917527152ca9f7e56cc8d3c5f5
aoconnor <oconnorails1@gmail.com>
2025-04-16 19:45:52 +0100

    car viewer html page started

--- car_dao_wsaa.py
+++ car_dao_wsaa.py
@@ -34,17 +34,42 @@ def get_car_by_reg(registration):
     cursor.close()
     return result
     
-def add_car(registration,make,model,colour,mileage,engineSize):
+def add_car(car):
     if conn is None:
         connect()
     query = "INSERT INTO car(registration,make,model,colour,mileage,engineSize) VALUES (%s, %s, %s, %s, %s, %s)"
+    values = (car.get("registration"), car.get("make"),car.get("model"),car.get("colour"),car.get("mileage"),car.get("engineSize"))
 
-    
     cursor = conn.cursor()
-    cursor.execute(query, (registration,make,model,colour, mileage, engineSize,))
+    cursor.execute(query, values)
     conn.commit()
     cursor.close()
     result = "Car added successfully" 
     return result
+
+def update_car(car):
+    if conn is None:
+        connect()
+    query = "UPDATE car SET make=%s, model=%s, colour=%s, mileage=%s, engineSize=%s  WHERE registration= %s"
+    values = (car.get("make"),car.get("model"),car.get("colour"),car.get("mileage"),car.get("engineSize"), car.get("registration"))
+
+    cursor = conn.cursor()
+    cursor.execute(query, values)
+    conn.commit()
+    result = "Car updated successfully"
+    cursor.close()
+    return result
+
+def delete_car(reg):
+    if conn is None:
+        connect()
+    query = "DELETE FROM CAR WHERE registration = %s"
+
+    cursor = conn.cursor()
+    cursor.execute(query, (reg,))
+    result = "Car deleted from database successfully"
+    cursor.close()
+    return result
+
 
 
--- server_cars.py
+++ server_cars.py
@@ -1,23 +1,62 @@
 from flask import Flask, request, jsonify
 import car_dao_wsaa
+import pymysql
 
 app = Flask(__name__, static_url_path="", static_folder="staticpages")
 
+@app.route("/", methods = ["GET"])
+def index():
+    return "<h1>AGHHHHHHHH</h1>"
+
 @app.route("/cars", methods = ["GET"])
-def getall():
+def get_all_cars():
     car_table = car_dao_wsaa.get_all_cars()
     return jsonify(car_table)
 
 @app.route("/cars/<reg>", methods = ["GET"])
 def get_car_by_reg(reg):
-    car = car_dao_wsaa.get_car_by_reg(reg) 
-    return jsonify(car)
+    try:
+        car = car_dao_wsaa.get_car_by_reg(reg) 
+        return jsonify(car)
+    except pymysql.err.IntegrityError as e:
+        return jsonify({"error": f"Car with registration {reg} not found"}), 404
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
-
+@app.route("/cars/<reg>", methods = ["DELETE"])
+def delete_car(reg):
+    try:
+         result = car_dao_wsaa.delete_car(reg)
+         return result
+    except pymysql.err.IntegrityError as e:
+        return jsonify({"error": f"Car with registration {reg} not found"}), 404
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
+    
 @app.route("/cars", methods = ["POST"])
-def add_a_car():
-    json_car = request.json
-    car_dao_wsaa.add_car(**json_car)
+def add_car():
+    try:
+         json_car = request.json
+         result = car_dao_wsaa.add_car(json_car)
+         return result
+    except pymysql.err.IntegrityError as e:
+        return jsonify({"error": f"Car with registration {json_car.get('registration')} already exists in database"}), 404
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@app.route("/cars", methods = ["PUT"])
+def update_car():
+    try:
+         json_car = request.json
+         result = car_dao_wsaa.update_car(json_car)
+         return result
+    except pymysql.err.IntegrityError as e:
+        return jsonify({"error": f"Car with registration {json_car.get('registration')} not found"}), 404
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
